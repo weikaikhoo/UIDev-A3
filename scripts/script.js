b40511cf37bb93c436927003ef9a220ca6ddcf90
weikaikhoo <weikaik@student.unimelb.edu.au>
2023-05-24 21:45:14 +1000

    <fixed the modules>

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,33 @@
-// const hamburger = document.querySelector("#hamburger");
+// const logo = document.querySelector("#logo");
 // const navbar = document.querySelector("#navbar");
 
-// hamburger.addEventListener("click", () =>{
-//     // hamburger.classlist.toggle("active");
-//     navbar.classList.toggle("show-nav");
-//     // document.getElementById("navbar").style.display ="block"
-// })
-
+// logo.addEventListener("click", () => {
+//   navbar.style.display = "none";
+// });
+// Hamburger menu toggle
 function showNavbar() {
     navbar = document.getElementById("navbar");
     if (navbar.style.display == "flex"){
         navbar.style.display = "none";
+        // navbar.anchor.addEventListener("click", () =>{
+        //     navbar.style.display = "none";
+        // })
     }
+
     else{
         navbar.style.display = "flex";
     }
   }
+
+//   Scrolling effect for anchor points
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+            if (target){
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+    });
+});
