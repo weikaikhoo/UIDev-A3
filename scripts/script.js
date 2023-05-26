835a9f11c0fb13143c224f1af413b53aa9229f1c
weikaikhoo <weikaik@student.unimelb.edu.au>
2023-05-27 00:11:00 +1000

    <temp menu change>

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,45 @@ window.onload = function(){
     // Hamburger menu toggle
     const hamburger = document.querySelector("#hamburger");
     const navbar = document.querySelector("#navbar");
-    const key = document.querySelector("navbtn");
+    const cart = document.querySelector("#cart");
 
-    hamburger.addEventListener("click", function() {
-        if (navbar.style.display == "flex"){
-            navbar.style.display = "none";
-        }
-        else{
-            navbar.style.display = "flex";
-        }
+    if(window.innerWidth < 768){
+        // document.write("blashkjhdskfs");
+        hamburger.addEventListener("click", function() {
+            if (navbar.style.display == "flex"){
+                navbar.style.display = "none";
+                hamburger.classList.remove("stick");
+                cart.classList.remove("stick");
+            }
+            else{
+                navbar.style.display = "flex";
+                // In order to stick the cart to the sidebar
+                hamburger.classList.toggle("stick");
+                cart.classList.toggle("stick");
+            }
         });
+        //   Scrolling effect for anchor points
+        document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+            anchor.addEventListener("click", function(e){
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute("href"));
+                    if (target){
+                        target.scrollIntoView({
+                            behavior: "smooth"
+                        });
+                    // When the anchors are clicked upon
+                    navbar.style.display ="none";
+                    hamburger.classList.toggle("stick");
+                    cart.classList.toggle("stick");
+                    }
+            });
+        });                
+    }
 
-        key.addEventListener("click", function(){
-            navbar.style.display = "none";
-        });
 
+if (window.innerWidth > 768){
+    // document.write("blashkjhdskfs");
+    navbar.style.display ="flex";
     //   Scrolling effect for anchor points
     document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
         anchor.addEventListener("click", function(e){
@@ -26,9 +50,15 @@ window.onload = function(){
                     target.scrollIntoView({
                         behavior: "smooth"
                     });
+                // When the anchors are clicked upon
+                // navbar.style.display ="flex";
+                // hamburger.classList.toggle("stick");
+                // cart.classList.toggle("stick");
                 }
         });
     });    
+}
+
 }
 
 
